e5696a1bf54d966347969d6ccb057c2f22a6ecf0
xuexiswmz <2635003975@qq.com>
2026-03-26 00:08:55 +0800

    fix: 恢复物料区热重载并移除antd Space弃用警告

--- src/materials/registry.ts
+++ src/materials/registry.ts
@@ -1,17 +1,39 @@
+import ButtonConfig from "./Button/config";
+import ContainerConfig from "./Container/config";
+import DividerConfig from "./Divider/config";
+import FormConfig from "./Form/config";
+import FormItemConfig from "./FormItem/config";
+import IconConfig from "./Icon/config";
+import ImageConfig from "./Image/config";
+import InputConfig from "./Input/config";
+import LinkConfig from "./Link/config";
+import ModalConfig from "./Modal/config";
+import PageConfig from "./Page/config";
+import TextConfig from "./Text/config";
 import type { ComponentConfig, ComponentConfigMap } from "./types";
 
-const configModules = import.meta.glob<{ default: ComponentConfig }>(
-  "./*/config.tsx",
-  { eager: true },
-);
+const builtinConfigs: ComponentConfig[] = [
+  ButtonConfig,
+  ContainerConfig,
+  DividerConfig,
+  FormConfig,
+  FormItemConfig,
+  IconConfig,
+  ImageConfig,
+  InputConfig,
+  LinkConfig,
+  ModalConfig,
+  PageConfig,
+  TextConfig,
+];
 
-export const builtinComponentConfig = Object.values(configModules).reduce<
-  ComponentConfigMap
->((acc, module) => {
-  const config = module.default;
-  acc[config.name] = config;
-  return acc;
-}, {});
+export const builtinComponentConfig = builtinConfigs.reduce<ComponentConfigMap>(
+  (acc, config) => {
+    acc[config.name] = config;
+    return acc;
+  },
+  {},
+);
 
 export function getComponentDefaultProps(config: ComponentConfig) {
   return {
